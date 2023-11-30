1600e0d1dbd5d107a5954053b2c86956ceafabac
Oh-hyeon Choung <ohhyeon.choung@gmail.com>
2023-11-30 22:45:00 +0000

    remove redundant functions

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,17 +1,29 @@
-import asyncio
-
 import os
-import async_timeout
+
 import openai
 from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import StreamingResponse
-from models import InferenceRequest
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from .models import InferenceRequest
 
 GENERATION_TIMEOUT_SEC = 60
 
 load_dotenv()
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api")
@@ -19,43 +31,13 @@ async def index():
     return {"message": "Welcome to vlib api!"}
 
 
-@app.post("/openai_streaming")
-async def openai_streaming(request: InferenceRequest) -> StreamingResponse:
-    try:
-        subscription = await openai.ChatCompletion.acreate(
-            model=request.model_name,
-            api_key=request.api_key,
-            organization=request.org_id,
-            messages=request.input_text,
-            stream=True,
-            **request.generation_cfg,
-        )
-
-        return StreamingResponse(
-            stream_generator(subscription), media_type="text/event-stream"
-        )
-    except openai.OpenAIError:
-        raise HTTPException(status_code=500, detail="OpenAI call failed")
-
-
-async def stream_generator(subscription):
-    async with async_timeout.timeout(GENERATION_TIMEOUT_SEC):
-        try:
-            async for chunk in subscription:
-                # post_processing can be any function that takes the chunk and returns a string
-                yield post_processing(chunk)
-        except asyncio.TimeoutError:
-            raise HTTPException(status_code=504, detail="Stream timed out")
-
-
 @app.post("/chat")
 async def chat(request: InferenceRequest):
     client = openai.AzureOpenAI(
-        api_key=os.getenv("OPENAI_API_KEY"),  
+        api_key=os.getenv("OPENAI_API_KEY"),
         api_version=os.getenv("OPENAI_API_VERSION"),
-        azure_endpoint=os.getenv("OPENAI_BASE_URL")
-        )
-
+        azure_endpoint=os.getenv("OPENAI_BASE_URL"),
+    )
     chat_completion = client.chat.completions.create(
         model=request.model_name,
         messages=[{"role": "user", "content": request.input_text}],
--- src/backend/models.py
+++ src/backend/models.py
@@ -1,11 +1,6 @@
-from typing import Optional
-
 from pydantic import BaseModel
 
 
 class InferenceRequest(BaseModel):
     model_name: str
     input_text: str
-    api_key: Optional[str]
-    org_id: Optional[str] = None
-    generation_cfg: Optional[dict] = None
