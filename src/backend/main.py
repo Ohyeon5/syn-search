eb1f4aec77e0a5ad0921e9f3477d27ea66e29755
Oh-hyeon Choung <ohhyeon.choung@gmail.com>
2023-12-25 13:16:30 +0100

    add rag prompt synthesizer

--- src/backend/main.py
+++ src/backend/main.py
@@ -52,7 +52,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "localhost:5173",
-    "https://proud-water-0f0a39703.4.azurestaticapps.net",
+    "https://proud-water-0f0a39703.4.azurestaticapps.net/",
 ]
 
 app.add_middleware(
